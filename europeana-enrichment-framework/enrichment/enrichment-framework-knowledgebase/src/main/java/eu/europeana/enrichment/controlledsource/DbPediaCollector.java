a81f38c879950cb0fe7e4b3376b841c2b3990070
cesare <cesare.concordia@isti.cnr.it>
2014-04-04 14:55:25 +0200

    DBPedia harvesting and mapping defined in #1223 impelemneted

--- europeana-enrichment-framework/enrichment/enrichment-framework-knowledgebase/src/main/java/eu/europeana/enrichment/controlledsource/DbPediaCollector.java
+++ europeana-enrichment-framework/enrichment/enrichment-framework-knowledgebase/src/main/java/eu/europeana/enrichment/controlledsource/DbPediaCollector.java
@@ -3,6 +3,11 @@ package eu.europeana.enrichment.controlledsource;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -25,21 +30,34 @@ import com.github.jsonldjava.core.JsonLdProcessor;
 import com.github.jsonldjava.jena.JenaJSONLD;
 import com.github.jsonldjava.jena.JenaRDFParser;
 
-import eu.europeana.enrichment.controlledsource.util.DataManager;;
+import eu.europeana.enrichment.controlledsource.util.DataManager;
+import eu.europeana.corelib.solr.entity.AgentImpl;
 
 public class DbPediaCollector {
 
 	 JenaRDFParser parser;
 	 DataManager dm = new DataManager();
 
+	 static int qLimit=100;
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		
 		DbPediaCollector dbpc= new DbPediaCollector();
+		
+		if (args!=null && args.length>0){
+			try{
+			qLimit = Integer.parseInt(args[0]);
+			}
+			catch (NumberFormatException nfe){
+				System.out.println ("*************WARNING in defining records limit in query answer, "+args[0]+" is not an int. Using default value: "+qLimit);
+			}
+		}
 		dbpc.loadAgentsfromDBPedia();
+		
 		//JenaJSONLD.init(); 
+		//dbpc.test();
 
 	}
 	public DbPediaCollector(){
@@ -54,8 +72,7 @@ public class DbPediaCollector {
 			dBuilder = dbFactory.newDocumentBuilder();
 			Document doc = dBuilder.parse(agentsRDF);
 			doc.getDocumentElement().normalize();
-			 
-			//System.out.println("Root element :" + doc.getDocumentElement().getNodeName()+"---");
+			
 			NodeList nList = doc.getElementsByTagName("rdf:Description");
 
 			for (int temp = 0; temp < nList.getLength(); temp++){
@@ -72,30 +89,26 @@ public class DbPediaCollector {
 			}
 		 
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 	 
-		//optional, but recommended
-		//read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
-		
 	}
 	
 	private void loadAgentsfromDBPedia(){
 		
 		try {
-			QueryEngineHTTP endpoint=new QueryEngineHTTP("http://dbpedia.org/sparql", "SELECT * WHERE {?subject ?y <http://dbpedia.org/ontology/Artist>.} LIMIT 10");
+			QueryEngineHTTP endpoint=new QueryEngineHTTP("http://dbpedia.org/sparql", "SELECT * WHERE {?subject ?y <http://dbpedia.org/ontology/Artist>.} LIMIT "+qLimit);//100");
 			System.out.println("getting artists from DBPedia ");
 			ResultSet rs= endpoint.execSelect();
-			System.out.println("exec query");
+			//System.out.println("exec query");
 			while (rs.hasNext()){
 				QuerySolution qs=rs.next();
 				
 				String subject=qs.get("subject").toString();
 				
 				//String z=qs.get("z").toString();
-				System.out.println(subject+"; ");
+				//System.out.println(subject+"; ");
 				//collectData(subject, "rdf");
 				collectAndMapControlledData(subject);
 			}
@@ -139,19 +152,22 @@ public class DbPediaCollector {
 		//read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
 		
 	}
-	
+	private void test(){
+		this.collectAndMapControlledData("http://dbpedia.org/resource/Albert_Gleizes");
+	}
 	private void collectAndMapControlledData(String key){
 
 
+		
 
 		QueryEngineHTTP endpoint=new QueryEngineHTTP("http://dbpedia.org/sparql", "describe <"+key+">");
 		System.out.println("describing "+key);
 		Model model=endpoint.execDescribe();
 		ByteArrayOutputStream baos= new ByteArrayOutputStream();
 		
-		RDFWriter writer= model.getWriter("RDF/XML-ABBREV");
+		//RDFWriter writer= model.getWriter("RDF/XML-ABBREV");
 		
-		
+		RDFWriter writer= model.getWriter("RDF/XML");
 		writer.setProperty("allowBadURIs", "true");
 		
 		writer.write(model, baos, null);
@@ -163,28 +179,36 @@ public class DbPediaCollector {
 			dBuilder = dbFactory.newDocumentBuilder();
 			Document doc = dBuilder.parse(new ByteArrayInputStream((baos.toByteArray())));
 
-			System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-			//NodeList nList = doc.getElementsByTagName("dcterms:subject");
-			NodeList nList = doc.getElementsByTagName("dbpprop:birthDate");
-			//System.out.println("---- "+key);
-
-			for (int temp = 0; temp < nList.getLength(); temp++) {
-
-				Node nNode = nList.item(temp);
-				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-					//NamedNodeMap nnm= nNode.getAttributes();
-					//Node no= nnm.getNamedItem("rdf:resource");
-
-					System.out.println(nNode.getFirstChild().getNodeValue());
-
-					
-					//ag.addWeakCandidate(key, no.getNodeValue());
-				}
-			}
+			//System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
 			
+			AgentImpl agent= new AgentImpl();
+			
+			agent.setAbout(key);
 
+			agent.setPrefLabel(getAgentProperty("dbpedia-owl:birthName", "dbpprop:birthName", doc ));
+			
+			
+			agent.setRdaGr2DateOfBirth(getAgentProperty("dbpedia-owl:birthDate", "dbpprop:birthDate", doc ));
+			agent.setRdaGr2BiographicalInformation(getAgentProperty("dbpedia-owl:abstract", "dbpprop:abstract", doc ));
+			agent.setRdaGr2ProfessionOrOccupation(getAgentProperty("dbpedia-owl:occupation", "dbpprop:occupation", doc));
 
-
+			agent.setRdaGr2DateOfDeath(getAgentProperty("dbpedia-owl:deathDate", "dbpprop:deathDate", doc));
+			agent.setEnd(getAgentProperty("dbpedia-owl:deathDate", "dbpprop:deathDate", doc));
+			agent.setFoafName(getAgentProperty("dbpedia-owl:birthName", "dbpprop:birthName", doc));
+			
+			agent.setAltLabel(getAgentProperty("dbpedia-owl:alternativaNames", "dbpprop:alternativaNames", doc));
+			
+			agent.setDcIdentifier(getAgentProperty("dbpedia-owl:viaf", "dbpprop:viaf", doc));
+			
+			Vector <String> tempsameAsattName= new Vector<String>();
+			tempsameAsattName.add("rdf:resource");
+			agent.setOwlSameAs(getAgentResource("owl:sameAs", "owl:sameAs", tempsameAsattName, doc));
+			
+			agent.setEdmIsRelatedTo(getAgentProperty("is dbpedia-owl:influenced of", "is dbprop:influenced of", doc));
+			agent.setEdmIsRelatedTo(getAgentProperty("is dbpedia-owl:influencedBy of", "is dbprop:influencedBy of", doc));
+			
+			dm.insertAgent(agent);
+						
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -194,6 +218,114 @@ public class DbPediaCollector {
 	
 	
 	}
+	
+	private HashMap <String,List<String>> getAgentProperty(String tag, String alternativeTag, Document doc){
+		HashMap<String,List<String>>  myM = new HashMap <String, List<String>>();
+		String logTag=tag;
+		NodeList nodeList = doc.getElementsByTagName(tag);
+		if (nodeList.getLength()==0 && alternativeTag!=null){
+			nodeList = doc.getElementsByTagName(alternativeTag);
+			logTag=alternativeTag;
+		}
+		String lang="def";
+		if (nodeList.getLength()>0)
+			System.out.println(logTag+" ("+ nodeList.getLength()+")");
+		for (int temp = 0; temp < nodeList.getLength(); temp++) {
+			
+
+			Node nNode = nodeList.item(temp);
+			if (nNode.getNodeType() == Node.ELEMENT_NODE && nNode.hasChildNodes()) {
+				NamedNodeMap nnm= nNode.getAttributes();
+				Node langAtt= nnm.getNamedItem("xml:lang");
+
+				Vector <String> date= new Vector <String>();
+				date.add(nNode.getFirstChild().getNodeValue());
+				
+				if (langAtt!=null && langAtt.hasChildNodes())
+					lang=langAtt.getFirstChild().getNodeValue();
+				
+				myM.put(lang, date);
+				System.out.println("  "+lang+", "+nNode.getFirstChild().getNodeValue());
+				//agent.setRdaGr2BiographicalInformation(myM);
+				
+			}
+			else{
+				if (nNode.getNodeType() == Node.ELEMENT_NODE){
+					Vector <String> attrName= new Vector <String> ();
+					attrName.add("rdf:resource");
+					Vector <String> attrValues=getElementResourceAttribute(nNode, attrName);
+					if (attrValues.size()>0){
+						myM.put(lang, attrValues);
+						System.out.println("  "+lang+", "+attrValues.toString());
+					}
+					
+				}
+			}
+		}
+		return myM;
+	}
+	
+	private String[] getAgentResource(String tag, String alternativeTag, List <String> attributes, Document doc){
+		
+		NodeList nodeList = doc.getElementsByTagName(tag);
+		ArrayList <String> result= new ArrayList <String>();
+		if (nodeList.getLength()==0 && alternativeTag!=null){
+			nodeList = doc.getElementsByTagName(alternativeTag);
+		}
+		
+		System.out.println(tag+"  ("+ nodeList.getLength()+", duplicates will be removed)");
+		for (int temp = 0; temp < nodeList.getLength(); temp++) {
+			
+			Node nNode = nodeList.item(temp);
+			//System.out.println(temp + " "+ nNode.toString()+" "+nNode.getNodeType());
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+				
+				NamedNodeMap nnm= nNode.getAttributes();
+				//testPrint(nnm);
+				for (String atts:attributes){
+					Node attValue= nnm.getNamedItem(atts);
+					if (attValue!=null && attValue.hasChildNodes()){
+						if (!result.contains(attValue.getFirstChild().getNodeValue())){
+							result.add(attValue.getFirstChild().getNodeValue());
+							System.out.println("  "+attValue.getFirstChild().getNodeValue());
+						}
+					}
+						
+				}
+				
+			}
+		}
+		return result.toArray(new String[result.size()]);
+	}
+	private void testPrint(NamedNodeMap nnm){
+		for ( int i=0; i< nnm.getLength(); i++){
+			System.out.println("ecco "+nnm.item(i));
+			
+		}
+	}
+	
+  private Vector <String> getElementResourceAttribute(Node nNode, List <String> attributes){
+		
+	  Vector <String> result= new Vector <String>();
+		
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+				NamedNodeMap nnm= nNode.getAttributes();
+				
+				for (String atts:attributes){
+					Node attValue= nnm.getNamedItem(atts);
+					if (attValue!=null && attValue.hasChildNodes()){
+						result.add(attValue.getFirstChild().getNodeValue());
+					}
+						
+				
+				
+				
+			}
+		}
+		return result;
+	}
+	
+	
 	private void collectData(String key, String serializationLang){
 
 
--- europeana-enrichment-framework/enrichment/enrichment-framework-knowledgebase/src/main/java/eu/europeana/enrichment/controlledsource/util/DataManager.java
+++ europeana-enrichment-framework/enrichment/enrichment-framework-knowledgebase/src/main/java/eu/europeana/enrichment/controlledsource/util/DataManager.java
@@ -1,10 +1,19 @@
 package eu.europeana.enrichment.controlledsource.util;
+import java.io.IOException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+
+import net.vz.mongodb.jackson.DBRef;
+import net.vz.mongodb.jackson.JacksonDBCollection;
+import net.vz.mongodb.jackson.WriteResult;
+
+import org.jibx.runtime.JiBXException;
  
-import com.mongodb.BasicDBObject;
-import com.mongodb.BasicDBObjectBuilder;
+
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
@@ -12,16 +21,115 @@ import com.mongodb.DBObject;
 import com.mongodb.Mongo;
 import com.mongodb.MongoException;
 import com.mongodb.util.JSON;
+
+import eu.europeana.corelib.solr.entity.AgentImpl;
+import eu.europeana.enrichment.api.internal.AgentTermList;
+import eu.europeana.enrichment.api.internal.ConceptTermList;
+import eu.europeana.enrichment.api.internal.MongoTerm;
+import eu.europeana.enrichment.api.internal.PlaceTermList;
+import eu.europeana.enrichment.api.internal.Term;
+import eu.europeana.enrichment.api.internal.TermList;
+import eu.europeana.enrichment.api.internal.TimespanTermList;
+
+import java.util.Map.Entry;
+
+
+//import eu.europeana.enrichment.api.internal.AgentTermList;
+//import eu.europeana.corelib.solr.entity.AgentImpl;
+
+
 public class DataManager {
 	
+	private static JacksonDBCollection<AgentTermList, String> aColl;
+	private static JacksonDBCollection<ConceptTermList, String> cColl;
+	private static JacksonDBCollection<PlaceTermList, String> pColl;
+	private static JacksonDBCollection<TimespanTermList, String> tColl;
 	Mongo mongo;
-	DB db;
+	private static DB db;
+	
 	public DataManager(){
 		try{
-			mongo = new Mongo("127.0.0.1",27017);
-			db = mongo.getDB("ControlledSource");
+		//	mongo = new Mongo("127.0.0.1",27017);
+		//	db = mongo.getDB("ControlledSource");
+			dbExists("127.0.0.1",27017);
 		}
 		catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	public static boolean dbExists(String host, int port) {
+		try {
+			if (db == null) {
+
+				Mongo mongo = new Mongo(host, port);
+				db = mongo.getDB("annocultor_db");
+				if (db.collectionExists("TermList")) {
+					cColl = JacksonDBCollection.wrap(
+							db.getCollection("TermList"),
+							ConceptTermList.class, String.class);
+
+					cColl.ensureIndex("codeUri");
+
+					aColl = JacksonDBCollection.wrap(
+							db.getCollection("TermList"), AgentTermList.class,
+							String.class);
+
+					aColl.ensureIndex("codeUri");
+					tColl = JacksonDBCollection.wrap(
+							db.getCollection("TermList"),
+							TimespanTermList.class, String.class);
+
+					tColl.ensureIndex("codeUri");
+					pColl = JacksonDBCollection.wrap(
+							db.getCollection("TermList"), PlaceTermList.class,
+							String.class);
+
+					pColl.ensureIndex("codeUri");
+
+					return true;
+				} else {
+					cColl = JacksonDBCollection.wrap(
+							db.getCollection("TermList"),
+							ConceptTermList.class, String.class);
+
+					cColl.ensureIndex("codeUri");
+
+					aColl = JacksonDBCollection.wrap(
+							db.getCollection("TermList"), AgentTermList.class,
+							String.class);
+
+					aColl.ensureIndex("codeUri");
+					tColl = JacksonDBCollection.wrap(
+							db.getCollection("TermList"),
+							TimespanTermList.class, String.class);
+
+					tColl.ensureIndex("codeUri");
+					pColl = JacksonDBCollection.wrap(
+							db.getCollection("TermList"), PlaceTermList.class,
+							String.class);
+
+					pColl.ensureIndex("codeUri");
+					return false;
+				}
+			}
+			return true;
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MongoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+	public void insertAgent(AgentImpl agent){
+		try {
+			agentToAgentTermList(agent);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JiBXException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -44,6 +152,48 @@ public class DataManager {
 			 e.printStackTrace();
 			 }		
 		 
+	}
+	
+	private static void agentToAgentTermList(AgentImpl agent) throws IOException, JiBXException {
+
+		AgentTermList termList = new AgentTermList();
+		termList.setCodeUri(agent.getAbout());
+		//If it had parents then we should get it now from the dcterms:isPartOf of the Concept and Place or the skos:broader
+		//of the Concept and append it here like
+		/**
+		 * String broader = placeImpl.getIsPartOf().get("def").get(0);
+		 * termList.setParent(broader);
+		 */
+
+		List<DBRef<? extends MongoTerm, String>> pList = new ArrayList<DBRef<? extends MongoTerm, String>>();
+
+		for (Entry<String, List<String>> prefLabel : agent.getPrefLabel().entrySet()) {
+			for (String label : prefLabel.getValue()) {
+				MongoTerm pTerm = new MongoTerm();
+				pTerm.setCodeUri(agent.getAbout());
+				pTerm.setLabel(label.toLowerCase());
+				String lang = prefLabel.getKey();
+
+				pTerm.setOriginalLabel(label);
+
+				pTerm.setLang(lang);
+
+				JacksonDBCollection<MongoTerm, String> pColl = JacksonDBCollection.wrap(db.getCollection("people"), MongoTerm.class, String.class);
+				pColl.ensureIndex("codeUri");
+				pColl.ensureIndex("label");
+				WriteResult<MongoTerm, String> res = pColl.insert(pTerm);
+				DBRef<MongoTerm, String> pTermRef = new DBRef<MongoTerm, String>(
+						res.getSavedObject().getId(), "people");
+				pList.add(pTermRef);
+			}
+		}
+
+		termList.setTerms(pList);
+
+		termList.setRepresentation(agent);
+		termList.setEntityType(AgentImpl.class.getSimpleName());
+		aColl.insert(termList);
+
 	}
 	
 
