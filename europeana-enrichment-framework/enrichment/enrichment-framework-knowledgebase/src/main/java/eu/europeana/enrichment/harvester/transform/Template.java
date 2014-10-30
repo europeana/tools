fc3b5d324074eff0d8a907c6dfbbd9a1778a5d3a
cesare <cesare.concordia@isti.cnr.it>
2014-10-30 14:19:27 +0100

    code added to remove invalid XML chars

--- europeana-enrichment-framework/enrichment/enrichment-framework-knowledgebase/src/main/java/eu/europeana/enrichment/harvester/transform/Template.java
+++ europeana-enrichment-framework/enrichment/enrichment-framework-knowledgebase/src/main/java/eu/europeana/enrichment/harvester/transform/Template.java
@@ -2,6 +2,7 @@ package eu.europeana.enrichment.harvester.transform;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
@@ -25,6 +26,8 @@ import javax.xml.stream.events.XMLEvent;
 import org.apache.commons.lang.StringUtils;
 
 import eu.europeana.corelib.solr.entity.ContextualClassImpl;
+
+import java.nio.charset.Charset;
 
 /**
  * Generic Template that converts the RDF representation of a contextual Class to Europeana compatible POJO
@@ -143,10 +146,68 @@ public abstract class Template<S extends ContextualClassImpl> {
      */
     protected S parse(S obj, String resourceUri, String xml,
             Map<String, String> methodMapping) {
-        InputStream in = new ByteArrayInputStream(xml.getBytes());
+    	 //System.out.println ("before "+ xml+"\n end before");
+    	
+    	
+    	
+    	//to be optimized
+    	xml=xml.replace("&#55296;", "");
+    	xml=xml.replace("&#55362;", "");
+    	xml=xml.replace("&#55363;", "");
+    	xml=xml.replace("&#55364;", "");
+    	xml=xml.replace("&#55378;", "");
+    	xml=xml.replace("&#55384;", "");
+    	xml=xml.replace("&#55391;", "");
+    	xml=xml.replace("&#55394;", "");
+    	xml=xml.replace("&#55629;", "");
+    	xml=xml.replace("&#56256;", "");
+    	xml=xml.replace("&#56322;", "");
+    	xml=xml.replace("&#56384;", "");
+    	xml=xml.replace("&#56394;", "");
+    	xml=xml.replace("&#56513;", "");
+    	xml=xml.replace("&#56648;", "");
+    	xml=xml.replace("&#56657;", "");
+    	xml=xml.replace("&#56786;", "");
+    	xml=xml.replace("&#57086;", "");
+    	xml=xml.replace("&#57113;", "");
+    	xml=xml.replace("&#57121;", "");
+    	xml=xml.replace("&#57136;", "");
+    	xml=xml.replace("&#57137;", "");
+    	xml=xml.replace("&#57138;", "");
+    	xml=xml.replace("&#57139;", "");
+    	xml=xml.replace("&#57140;", "");
+    	xml=xml.replace("&#57142;", "");
+    	xml=xml.replace("&#57143;", "");
+    	xml=xml.replace("&#57145;", "");
+    	xml=xml.replace("&#57146;", "");
+    	xml=xml.replace("&#57147;", "");
+    	xml=xml.replace("&#57148;", "");
+    	xml=xml.replace("&#57149;", "");
+    	xml=xml.replace("&#57150;", "");
+    	xml=xml.replace("&#57151;", "");
+    	xml=xml.replace("&#57152;", "");
+    	xml=xml.replace("&#57154;", "");
+    	xml=xml.replace("&#57155;", "");
+    	xml=xml.replace("&#57156;", "");
+    	xml=xml.replace("&#57157;", "");
+    	xml=xml.replace("&#57158;", "");
+    	xml=xml.replace("&#57159;", "");
+    	xml=xml.replace("&#57161;", "");
+    	xml=xml.replace("&#57279;", "");
+    	//
+    	
+    	XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+    	inputFactory.setProperty(XMLInputFactory.IS_REPLACING_ENTITY_REFERENCES, false);
+    	inputFactory.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false);
+    	InputStream in = new ByteArrayInputStream(xml.getBytes(Charset.forName("UTF-8")));
+    	//String xml10pattern = "[^"
+        //+ "\u0009\r\n"
+        //+ "\u0020-\uD7FF"
+        //+ "\uE000-\uFFFD"
+        //+ "\ud800\udc00-\udbff\udfff"
+        //+ "]";
         try {
-            XMLEventReader eventReader = XMLInputFactory.newInstance()
-                    .createXMLEventReader(in);
+            XMLEventReader eventReader = inputFactory.createXMLEventReader(in);
             boolean isRoot = true;
             while (eventReader.hasNext()) {
                 XMLEvent event = eventReader.nextEvent();
@@ -154,6 +215,7 @@ public abstract class Template<S extends ContextualClassImpl> {
                     StartElement startElement = event.asStartElement();
                     String qElem = startElement.getName().getPrefix() + ":"
                             + startElement.getName().getLocalPart();
+                   
                     if (methodMapping.containsKey(qElem)) {
                         if (isRoot) {
                             appendValue(obj, methodMapping.get(qElem),
@@ -183,13 +245,15 @@ public abstract class Template<S extends ContextualClassImpl> {
                                         attribute.setAttributeValue(langAttr
                                                 .getValue());
                                     }
-                                    event = eventReader.nextEvent();
+                                   
+                                    	event = eventReader.nextEvent();
                                     if (!event.isEndElement()) {
-                                        attribute.setElementValue(event
+                                    	 attribute.setElementValue(event
                                                 .asCharacters().getData());
                                         appendValue(obj, methodMapping.get(qElem),
                                                 attribute);
                                     }
+                                    
                                 }
                             }
                         }
@@ -199,7 +263,7 @@ public abstract class Template<S extends ContextualClassImpl> {
             }
             return obj;
         } catch (XMLStreamException | FactoryConfigurationError e) {
-            log.log(Level.SEVERE, e.getMessage());
+            log.log(Level.SEVERE, e.getMessage()+" \n"+xml);
         }
         return null;
     }
