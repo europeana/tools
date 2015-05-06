0d77d8dcf83ff850fbb39debe80f2463bc9961a3
ymamakis <yorgos.mamakis@kb.nl>
2015-05-06 21:39:10 +0200

    Ensured resume functionality and correct finalization

--- migration/src/main/java/eu/europeana/migration/Migration.java
+++ migration/src/main/java/eu/europeana/migration/Migration.java
@@ -5,24 +5,17 @@
  */
 package eu.europeana.migration;
 
-import com.mongodb.Mongo;
-import com.mongodb.ServerAddress;
-import eu.europeana.corelib.edm.exceptions.MongoDBException;
-import eu.europeana.corelib.edm.utils.construct.FullBeanHandler;
-import eu.europeana.corelib.edm.utils.construct.SolrDocumentHandler;
-import eu.europeana.corelib.mongo.server.EdmMongoServer;
-import eu.europeana.corelib.mongo.server.impl.EdmMongoServerImpl;
-import eu.europeana.corelib.solr.bean.impl.FullBeanImpl;
+import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.net.MalformedURLException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.CloudSolrServer;
@@ -31,8 +24,16 @@ import org.apache.solr.client.solrj.impl.LBHttpSolrServer;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
-import org.apache.solr.common.SolrInputDocument;
 import org.apache.solr.common.params.CursorMarkParams;
+
+import com.mongodb.Mongo;
+import com.mongodb.ServerAddress;
+
+import eu.europeana.corelib.edm.exceptions.MongoDBException;
+import eu.europeana.corelib.edm.utils.construct.FullBeanHandler;
+import eu.europeana.corelib.edm.utils.construct.SolrDocumentHandler;
+import eu.europeana.corelib.mongo.server.EdmMongoServer;
+import eu.europeana.corelib.mongo.server.impl.EdmMongoServerImpl;
 
 /**
  *
@@ -83,7 +84,9 @@ public class Migration {
             params.setSort(fl, SolrQuery.ORDER.asc);
             params.setFields(fl);
             String cursorMark = CursorMarkParams.CURSOR_MARK_START;
-
+            if(new File("querymark").exists()){
+            	cursorMark = FileUtils.readFileToString(new File("querymark"));
+            }
             boolean done = false;
             int i = 0;
             while (!done) {
@@ -97,6 +100,7 @@ public class Migration {
                     done = true;
                 }
                 cursorMark = nextCursorMark;
+                FileUtils.write(new File("querymark"), cursorMark, false);
                 i += 10000;
                 Logger.getLogger(Migration.class.getName()).log(Level.INFO, "Added " + i + " documents");
             }
@@ -105,45 +109,19 @@ public class Migration {
             Logger.getLogger(Migration.class.getName()).log(Level.SEVERE, null, ex);
         } catch (MalformedURLException ex) {
             Logger.getLogger(Migration.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        } catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
     }
 
     private static void doCustomProcessingOfResults(QueryResponse resp) {
-//        List<SolrInputDocument> docList = new ArrayList<>();
-//        for (SolrDocument doc : resp.getResults()) {
-//            String id = doc.getFieldValue("europeana_id").toString();
-//            if (id.startsWith("//")) {
-//                id = id.replace("//", "/");
-//            }
-//            try {
-//                
-//                long read = new Date().getTime();
-//                FullBeanImpl fBean = (FullBeanImpl) sourceMongo.getFullBean(id);
-//                
-//               // Logger.getLogger(Migration.class.getName()).log(Level.INFO, "Reading took "+ (new Date().getTime()-read) + " ms");
-//                SolrInputDocument inputDoc = solrHandler.generate(fBean);
-//               docList.add(inputDoc);
-//               long write =  new Date().getTime();
-//               mongoHandler.saveEdmClasses(fBean, true);
-//               targetMongo.getDatastore().save(fBean);
-//            //    Logger.getLogger(Migration.class.getName()).log(Level.INFO, "Writing took "+ (new Date().getTime()-write) + " ms");
-//            } catch (MongoDBException | SolrServerException | NoSuchMethodException | IllegalAccessException | InvocationTargetException ex) {
-//                Logger.getLogger(Migration.class.getName()).log(Level.SEVERE, null, ex);
-//            }
-//            
-//        }
-//        
-//        try {
-//            targetSolr.add(docList);
-//        } catch (SolrServerException | IOException ex) {
-//            Logger.getLogger(Migration.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-        
+
+        if(resp.getResults().size()==10000){
         List<List<SolrDocument>> segments = segment(resp.getResults());
         CountDownLatch latch = new CountDownLatch(50);
         
-   //     initializeThreads(segments,latch);
         for(List<SolrDocument> segment:segments){
             ReadWriter writer = new ReadWriter();
             writer.setCloudServer(targetSolr);
@@ -161,7 +139,25 @@ public class Migration {
         } catch (InterruptedException ex) {
             Logger.getLogger(Migration.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+        } else {
+        	CountDownLatch latch = new CountDownLatch(1);
+        	ReadWriter writer = new ReadWriter();
+            writer.setCloudServer(targetSolr);
+            writer.setSegment(resp.getResults());
+            writer.setSolrHandler(solrHandler);
+            writer.setSourceMongo(sourceMongo);
+            writer.setTargetMongo(targetMongo);
+            writer.setfBeanHandler(mongoHandler);
+            writer.setLatch(latch);
+            Thread t = new Thread(writer);
+            t.start();
+            try {
+				latch.await();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        }
         
     }
 
