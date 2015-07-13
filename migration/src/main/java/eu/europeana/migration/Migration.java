2705958abc73aa48db75e26149e0f258e83f15ab
ymamakis <yorgos.mamakis@kb.nl>
2015-07-13 19:25:57 +0200

    Modified for collection 2022333

--- migration/src/main/java/eu/europeana/migration/Migration.java
+++ migration/src/main/java/eu/europeana/migration/Migration.java
@@ -70,7 +70,7 @@ public class Migration {
             production = Target.PRODUCTION;
             
             //Query to the source
-            String query = "europeana_collectionName:2022304*";
+            String query = "europeana_collectionName:2022333_*";
             String fl = "europeana_id";
             SolrQuery params = new SolrQuery();
             params.setQuery(query);
